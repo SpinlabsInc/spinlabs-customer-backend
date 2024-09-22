15836971be194b303b1513605821fe060f3934ad
EZClean_app <ksteja99@gmail.com>
2024-09-22 00:23:15 -0400

    working tests

--- Services/tests/test_main.py
+++ Services/tests/test_main.py
@@ -1,14 +1,7 @@
-import sys
-import os
 import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import patch
-from ..src.main import app  # Use relative import
-
-
-# Add the absolute path to the `src` directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-
+from src.main import app  # Use absolute import, assuming src is the top-level directory
 
 client = TestClient(app)
 
